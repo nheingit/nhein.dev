8d493ad0731ac99660a4252a25178d13188785b8
Noah <nheindev@gmail.com>
2022-09-05 14:49:17 -0500

    changed copyright in footer

--- src/partials/Footer.tsx
+++ src/partials/Footer.tsx
@@ -1,10 +1,18 @@
-import { FooterCopyright, Section } from 'astro-boilerplate-components';
+import { Section } from 'astro-boilerplate-components';
 
 import { AppConfig } from '@/utils/AppConfig';
 
 const Footer = () => (
   <Section>
-    <FooterCopyright site_name={AppConfig.site_name} />
+    <div className="border-t border-gray-600 pt-5">
+      <div className="text-sm text-gray-200">
+        {[
+          `\u00A9 Copyright${new Date().getFullYear()}`,
+          ' by ',
+          AppConfig.site_name,
+        ]}
+      </div>
+    </div>
   </Section>
 );
 
