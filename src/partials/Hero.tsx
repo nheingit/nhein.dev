27ebf7d148e70cb7513b522f6a72f58a127a3820
Noah <nheindev@gmail.com>
2022-09-10 20:09:25 -0500

    updated all post images, and removed the useless projects on the homepage

--- src/partials/Hero.tsx
+++ src/partials/Hero.tsx
@@ -15,34 +15,47 @@ const Hero = () => (
       }
       description={
         <>
-        I'm a Developer Advocate, which means a lot of different things, to many people.
-          Mainly I produce technical content for developers to educate them on both high-level concepts, and the
-          {' '}
-          <a className="text-cyan-400 hover:underline" target="_blank" href="https://egghead.io/courses/introduction-to-smart-contracts-with-ethereum-and-solidity-0a40bba0">
+          I'm a Developer Advocate, which means a lot of different things, to
+          many people. Mainly I produce technical content for developers to
+          educate them on both high-level concepts, and the{' '}
+          <a
+            className="text-cyan-400 hover:underline"
+            target="_blank"
+            href="https://egghead.io/courses/introduction-to-smart-contracts-with-ethereum-and-solidity-0a40bba0"
+          >
             nitty gritty
           </a>
-          .
-          Right now I work in the Blockchain space, trying to build a world where access to a global financial system only requires an internet connection.
-          I work at{' '}
-          <a className="text-cyan-400 hover:underline" href="https://phantom.app" target="_blank">
+          . Right now I work in the Blockchain space, trying to build a world
+          where access to a global financial system only requires an internet
+          connection. I work at{' '}
+          <a
+            className="text-cyan-400 hover:underline"
+            href="https://phantom.app"
+            target="_blank"
+          >
             Phantom
-          </a>
-          {' '}
-          currently. Before that, I lead the DevRel team at {' '}
-          <a className="text-cyan-400 hover:underline" target="_blank" href="https://quicknode.com">
+          </a>{' '}
+          currently. Before that, I lead the DevRel team at{' '}
+          <a
+            className="text-cyan-400 hover:underline"
+            target="_blank"
+            href="https://quicknode.com"
+          >
             QuickNode
           </a>
-          .
-          I'm also a proud 
-          {' '}
-          <a className="text-cyan-400 hover:underline" target="_blank" href="https://egghead.io/q/resources-by-noah-hein">
+          . I'm also a proud{' '}
+          <a
+            className="text-cyan-400 hover:underline"
+            target="_blank"
+            href="https://egghead.io/q/resources-by-noah-hein"
+          >
             Egghead Instructor
           </a>
           .
         </>
       }
       avatar={
-      <img
+        <img
           className="h-80 w-64"
           src="/assets/images/lowpoly-man-removebg-preview.png"
           alt="Avatar image"
--- src/partials/Navbar.tsx
+++ src/partials/Navbar.tsx
@@ -1,42 +1,65 @@
-import { Logo, NavbarTwoColumns, NavMenu, Section } from 'astro-boilerplate-components';
-import { Hits, Hit, SearchBox } from './Search';
 import algoliasearch from 'algoliasearch';
+import {
+  Logo,
+  NavbarTwoColumns,
+  NavMenu,
+  Section,
+} from 'astro-boilerplate-components';
 import { InstantSearch } from 'react-instantsearch-hooks';
 
-const Navbar = () => {
-	const searchClient = algoliasearch(import.meta.env.PUBLIC_ALGOLIA_INDEX, import.meta.env.PUBLIC_ALGOLIA_SEARCHKEY);
-	return (
-		<InstantSearch searchClient={searchClient} indexName="Blog">
-			<div className="sticky bg-slate-900 z-10 top-0">
-				<Section>
-					<NavbarTwoColumns>
-						<a href="/">
-							<Logo
-								icon={
-                  <img className='h-12 w-12' alt='logo with the letters NH' src='/assets/images/NHLogo.png' />
-								}
-								name="Noah's Digital Garden"
-							/>
-						</a>
+import { Hit, Hits, SearchBox } from './Search';
 
-						<NavMenu>
-							<a className="mt-2" href="/posts">Garden</a>
-							<a  className="mt-2" href="https://github.com/nheingit" target="_blank">
-								GitHub
-							</a>
-							<a className="mt-2" href="https://twitter.com/nheindev" target="_blank">
-								Twitter
-							</a>
+const Navbar = () => {
+  const searchClient = algoliasearch(
+    import.meta.env.PUBLIC_ALGOLIA_INDEX,
+    import.meta.env.PUBLIC_ALGOLIA_SEARCHKEY
+  );
+  return (
+    <InstantSearch searchClient={searchClient} indexName="Blog">
+      <div className="sticky top-0 z-10 bg-slate-900">
+        <Section>
+          <NavbarTwoColumns>
+            <a href="/">
+              <Logo
+                icon={
+                  <img
+                    className="h-12 w-12"
+                    alt="logo with the letters NH"
+                    src="/assets/images/NHLogo.png"
+                  />
+                }
+                name="Noah's Digital Garden"
+              />
+            </a>
+
+            <NavMenu>
+              <a className="mt-2" href="/posts">
+                Garden
+              </a>
+              <a
+                className="mt-2"
+                href="https://github.com/nheingit"
+                target="_blank"
+              >
+                GitHub
+              </a>
+              <a
+                className="mt-2"
+                href="https://twitter.com/nheindev"
+                target="_blank"
+              >
+                Twitter
+              </a>
               <div>
                 <SearchBox />
                 <Hits hitComponent={Hit} />
               </div>
-						</NavMenu>
-					</NavbarTwoColumns>
-				</Section>
-			</div>
-		</InstantSearch>
-	);
+            </NavMenu>
+          </NavbarTwoColumns>
+        </Section>
+      </div>
+    </InstantSearch>
+  );
 };
 
 export { Navbar };
